1b96716cbfbcd94db79fa674435de2a016c94a89
Oscar Chan <oc.ideas@gmail.com>
2012-08-03 18:05:56 -0700

    set up the sign up page with css

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,9 +1,20 @@
 class UsersController < ApplicationController
   def new
+    @user = User.new
     @title = "Sign up"
   end
 
   def show
     @user = User.find(params[:id])
+  end
+
+  def create
+    @user = User.new(params[:user])
+    if @user.save
+      #
+    else
+      @title = "Sign Up"
+      render 'new'
+    end
   end
 end
--- spec/controllers/users_controller_spec.rb
+++ spec/controllers/users_controller_spec.rb
@@ -16,14 +16,37 @@ describe UsersController do
   end
 
   describe "Get 'new'" do
+    before :each do
+      @attr = {
+          name: "",
+          email: "",
+          password: "",
+          password_confirmation: ""
+      }
+    end
+
     it "should be successful" do
-      get 'new'
+      get :new
       response.should be_success
     end
 
     it "should have the right title" do
-      get 'new'
-      response.should have_selector("title", content:"Ruby on Rails Tutorial Sample App | Sign up")
+      get :new
+      response.should have_selector("title", content: "Ruby on Rails Tutorial Sample App | Sign up")
+    end
+
+    it "should not accept an empty form" do
+      get :new
+
+      lambda {
+        post :create, :user => @attr
+      }.should_not change(User, :count)
+    end
+
+    it "should render the 'new' page" do
+      post :create, :user => @attr
+      response.should render_template('new')
     end
   end
+
 end
