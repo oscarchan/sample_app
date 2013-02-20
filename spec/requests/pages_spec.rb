4193ab3f0b72a5a9257f1d654833c362e1937e1a
Oscar Chan <oscarc@surveymonkey.com>
2013-02-20 14:25:32 -0800

    completed chapter 3.2

--- spec/requests/pages_spec.rb
+++ spec/requests/pages_spec.rb
@@ -5,10 +5,7 @@ describe "Pages", type: :feature do
     before(:each) { visit '/pages/home' }
     subject { page }
 
-    it { should have_content('Sample App') }
-
-    # bug reference: https://github.com/jnicklas/capybara/issues/844
-
+    it { should have_selector('h1', text: "Sample App") }
     it { should have_selector('title', text: "| Home") }
   end
 
@@ -16,14 +13,14 @@ describe "Pages", type: :feature do
     before(:each) { visit '/pages/help' }
     subject { page }
 
-    it { should have_content('Help') }
+    it { should have_selector('h1', text: "Help") }
     it { should have_selector('title', text: "| Help") }
   end
 
   context "About page" do
     before(:each) { visit '/pages/about' }
     subject { page }
-    it { should have_content('About Us') }
+    it { should have_selector('h1', text: 'About Us') }
     it { should have_selector('title', text: "| About") }
   end
 end
