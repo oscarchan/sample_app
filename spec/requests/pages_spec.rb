16ff0d00c3fe260311f4c01b3d5232f031282649
Oscar Chan <oscarc@surveymonkey.com>
2013-02-28 23:50:30 -0800

    completed chapter 5.3

--- spec/requests/pages_spec.rb
+++ spec/requests/pages_spec.rb
@@ -2,15 +2,16 @@ require 'spec_helper'
 
 describe "Pages", type: :feature do
   context "Home page" do
-    before(:each) { visit '/pages/home' }
+    before(:each) { visit home_path }
     subject { page }
 
     it { should have_selector('h1', text: "Sample App") }
-    it { should have_selector('title', text: "| Home") }
+    it { should have_selector('title', text: full_title('')) }
+    it { should_not have_selector('title', text: "| Home") }
   end
 
   context "Help page" do
-    before(:each) { visit '/pages/help' }
+    before(:each) { visit help_path }
     subject { page }
 
     it { should have_selector('h1', text: "Help") }
@@ -18,16 +19,17 @@ describe "Pages", type: :feature do
   end
 
   context "About page" do
-    before(:each) { visit '/pages/about' }
+    before(:each) { visit about_path }
     subject { page }
     it { should have_selector('h1', text: 'About Us') }
     it { should have_selector('title', text: "| About") }
   end
 
   context "Contact page" do
-    before(:each) { visit '/pages/contact' }
+    before(:each) { visit contact_path }
     subject { page }
 
     it { should have_selector('h1', text: 'Contact Us') }
-    it { should have_selector('title', text: "| Contact") }  end
+    it { should have_selector('title', text: "| Contact") }
+  end
 end
