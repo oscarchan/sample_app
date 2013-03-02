1cac2cd7f92b92c1dd141ed7e3ee9f0c01836311
Oscar Chan <oscarc@surveymonkey.com>
2013-03-01 17:19:25 -0800

    updated with shared examples

--- spec/requests/pages_spec.rb
+++ spec/requests/pages_spec.rb
@@ -1,43 +1,43 @@
 require 'spec_helper'
 
 describe "Pages", type: :feature do
+  subject { page }  # page is a singleton session
+
+  shared_examples "a common page" do |page_heading, page_title|
+      it { should have_selector('h1', text: page_heading) }
+      it { should have_selector('title', text: full_title(page_title)) }
+  end
+
 
   context "Home page" do
     before(:each) { visit home_path }
-    subject { page }
 
-    it { should have_selector('h1', text: "Sample App") }
-    it { should have_selector('title', text: full_title('')) }
+    it_should_behave_like "a common page", 'Sample App', ''
+
     it { should_not have_selector('title', text: "| Home") }
   end
 
   context "Help page" do
     before(:each) { visit help_path }
-    subject { page }
 
-    it { should have_selector('h1', text: "Help") }
-    it { should have_selector('title', text: "| Help") }
+    it_should_behave_like "a common page", 'Help', 'Help'
   end
 
   context "About page" do
     before(:each) { visit about_path }
-    subject { page }
-    it { should have_selector('h1', text: 'About Us') }
-    it { should have_selector('title', text: "| About") }
+
+    it_should_behave_like "a common page", 'About Us', 'About'
   end
 
   context "Contact page" do
     before(:each) { visit contact_path }
-    subject { page }
 
-    it { should have_selector('h1', text: 'Contact Us') }
-    it { should have_selector('title', text: "| Contact") }
+    it_should_behave_like "a common page", 'Contact Us', 'Contact'
   end
 
   context "Sign up page" do
     before(:each) { visit signup_path }
-    subject { page }
-    it { should have_selector('h1', text: 'Sign up')}
-    it { should have_selector('title', text: "Sign up")}
+
+    it_should_behave_like "a common page", 'Sign up', 'Sign up'
   end
 end
